5ca8f35eb4182c3649db787e45fd408f14296f64
remi42000 <rgdu42@yandex.com>
2021-01-17 19:33:10 +0100

    electric car

--- learn.js
+++ learn.js
@@ -1,3 +1,13 @@
-var message = 'Hello Worldd';
-
-console.log(message);
+var Car = /** @class */ (function () {
+    function Car(model, doors, isElectric) {
+        this.model = model;
+        this.doors = doors;
+        this.isElectric = isElectric;
+    }
+    Car.prototype.make = function () {
+        console.log("This car is " + this.model + " which has " + this.doors + " doors");
+    };
+    return Car;
+}());
+var newCar = new Car('Innova', 4, false);
+newCar.make();
--- learn.ts
+++ learn.ts
@@ -1,2 +1,17 @@
-let message: string = 'Hello World';
-console.log(message);
+class Car {
+    model: String;
+    doors: Number;
+    isElectric: Boolean;
+    
+    constructor(model: String, doors: Number, isElectric: Boolean) {
+        this.model = model;
+        this.doors = doors;
+        this.isElectric = isElectric;
+    }
+
+    make(): void {
+        console.log(`This car is ${this.model} which has ${this.doors} doors` );
+    }
+}
+let newCar = new Car('Innova', 4, false);
+newCar.make();
